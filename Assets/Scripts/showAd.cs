66cffbb69c8872ebb1015084e07e83eaeffae571
Royalcows9 <Royalcows9@gmail.com>
2016-11-22 17:54:33 -0800

    Added ability to show an ad

---
+++ Assets/Scripts/showAd.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class showAd : MonoBehaviour
+{
+    void Start()
+    {
+        if (Advertisement.IsReady())
+        {
+            Advertisement.Show();
+        }
+    }
+}
